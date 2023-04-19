2080d46bd3c2a2334e3f4412ca9326e6c366a559
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-04-19 22:52:56 +0200

    3 creating package sructure

--- src/test/java/pl/zajavka/integration/CarDealershipTest.java
+++ src/test/java/pl/zajavka/integration/CarDealershipTest.java
@@ -1,47 +1,54 @@
 package pl.zajavka.integration;
 
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
+@Slf4j
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CarDealershipTest {
 
     @Test
     @Order(1)
     void purge() {
+        log.info("### RUNING ORDER 1");
 
     }
 
     @Test
     @Order(2)
     void init() {
+        log.info("### RUNING ORDER 2");
 
     }
 
     @Test
     @Order(3)
     void purchase() {
-
-    }
-
-    @Test
-    @Order(3)
-    void makeServiceRequest() {
+        log.info("### RUNING ORDER 3");
 
     }
 
     @Test
     @Order(4)
-    void processServiceRequest() {
+    void makeServiceRequest() {
+        log.info("### RUNING ORDER 4");
 
     }
 
     @Test
     @Order(5)
+    void processServiceRequest() {
+        log.info("### RUNING ORDER 5");
+
+    }
+
+    @Test
+    @Order(6)
     void printCarHistory() {
+        log.info("### RUNING ORDER 6");
 
     }
 }
