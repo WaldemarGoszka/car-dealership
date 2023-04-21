78e42c3c009dd9a65b3b5d162cb3894850bf12b4
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-04-21 12:50:43 +0200

    4 add few entity configuration

--- src/main/java/pl/zajavka/infrastructure/database/entity/CarToBuyEntity.java
+++ src/main/java/pl/zajavka/infrastructure/database/entity/CarToBuyEntity.java
@@ -1,4 +1,43 @@
 package pl.zajavka.infrastructure.database.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "carToBuyId")
+@ToString(of = {"carToBuyId", "vin", "brand", "model", "year"})
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "car_to_buy")
 public class CarToBuyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "car_to_buy")
+    private Integer carToBuyId;
+
+    @Column(name = "vin", unique = true)
+    private String vin;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "year")
+    private Integer year;
+
+    @Column(name = "color")
+    private String color;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "car")
+    private InvoiceEntity invoice;
 }
