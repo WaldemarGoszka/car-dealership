ad08b139b33b1fbe2753553011d90a5a40a6cdb7
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-04-22 12:33:33 +0200

    5 start creating file .md mapping

---
+++ src/main/java/pl/zajavka/business/management/InputDataCashe.java
@@ -0,0 +1,8 @@
+package pl.zajavka.business.management;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class InputDataCashe {
+    public static final String FILE_PATH = ".src/main/resources/40-car-dealership-traffic-simulation.md";
+}
