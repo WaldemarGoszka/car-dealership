cb0381883d906ba4f250c7da564d72acf6ccd023
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-04-25 13:25:05 +0200

    6 file mapping

--- src/main/java/pl/zajavka/business/management/InputDataCashe.java
+++ src/main/java/pl/zajavka/business/management/InputDataCashe.java
@@ -2,7 +2,43 @@ package pl.zajavka.business.management;
 
 import lombok.experimental.UtilityClass;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @UtilityClass
 public class InputDataCashe {
     public static final String FILE_PATH = ".src/main/resources/40-car-dealership-traffic-simulation.md";
+
+    private static final Map<String, List<String>> inputData;
+
+    static{
+        try {
+            inputData = readFileContent();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    private static Map<String, List<String>> readFileContent() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(FILE_PATH)).stream()
+                .filter(line -> line.startsWith("[//]: #"))
+                .filter(line -> line.isBlank())
+                .toList();
+
+        return lines.stream()
+                .collect(Collectors.groupingBy(
+                        line -> line.split("->")[0].trim(),
+                        Collectors.mapping(
+                                line -> line.substring(line.indexOf("->") + 2).trim(),
+                                Collectors.toList()
+                        )
+                ));
+    }
+    public static Map<String, List<String>> getInputData{
+        return inputData;
+    }
 }
